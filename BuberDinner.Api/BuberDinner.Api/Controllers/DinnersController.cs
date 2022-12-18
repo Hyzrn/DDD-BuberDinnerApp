eaf966db7b3aa943888bb49d4de333349e8b41d5
shayzaran <shayzaran@gmail.com>
2022-12-18 13:05:54 +0300

    Added Authentication and Authorization middlewares

---
+++ BuberDinner.Api/BuberDinner.Api/Controllers/DinnersController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers
+{
+    [Route("[controller]")]
+    public class DinnersController : ApiController
+    {
+        [HttpGet]
+        public IActionResult ListDinners()
+        {
+            return Ok(Array.Empty<string>());
+        }
+    }
+} 
--- BuberDinner.Api/BuberDinner.Api/DependencyInjection.cs
+++ BuberDinner.Api/BuberDinner.Api/DependencyInjection.cs
@@ -1,6 +1,7 @@
 ﻿using BuberDinner.Api.Common.Errors;
 using BuberDinner.Api.Common.Mapping;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.OpenApi.Models;
 
 namespace BuberDinner.Api
 {
@@ -11,9 +12,40 @@ namespace BuberDinner.Api
         {
             services.AddControllers();
             services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
+            services.AddSwagger();
             services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
             services.AddMappings();
+
+            return services;
+        }
+
+        public static IServiceCollection AddSwagger(
+            this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo {
+                    Title = "BubberDinnerAPI", Version = "v1"
+                });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme() {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme.",
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                    {
+                        new OpenApiSecurityScheme {
+                            Reference = new OpenApiReference {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
 
             return services;
         }
